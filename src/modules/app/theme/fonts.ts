4fd19c6e4886fd8b89fb5b38b4a45493d3c4b181
Maciej Baczynski <maciej.baczynski@ordergroup.pl>
2025-06-30 12:03:21 +0200

    "Arial" is not a system font fix

--- src/modules/app/theme/fonts.ts
+++ src/modules/app/theme/fonts.ts
@@ -1,102 +1,102 @@
 import { ThemeProp } from 'react-native-paper/lib/typescript/types';
 
-const arial = 'Arial';
+const fontFamily = 'sans-serif';
 
 export const fonts: ThemeProp['fonts'] = {
   default: {
-    fontFamily: arial,
+    fontFamily: fontFamily,
   },
   // display
   displayLarge: {
-    fontFamily: arial,
+    fontFamily: fontFamily,
     fontSize: 57,
     letterSpacing: -0.25,
     lineHeight: 64,
   },
   displayMedium: {
-    fontFamily: arial,
+    fontFamily: fontFamily,
     fontSize: 45,
     letterSpacing: 0,
     lineHeight: 33,
   },
   displaySmall: {
-    fontFamily: arial,
+    fontFamily: fontFamily,
     fontSize: 36,
     letterSpacing: 0,
     lineHeight: 44,
   },
   // headline
   headlineLarge: {
-    fontFamily: arial,
+    fontFamily: fontFamily,
     fontSize: 32,
     letterSpacing: 0,
     lineHeight: 40,
   },
   headlineMedium: {
-    fontFamily: arial,
+    fontFamily: fontFamily,
     fontSize: 28,
     letterSpacing: 0,
     lineHeight: 36,
   },
   headlineSmall: {
-    fontFamily: arial,
+    fontFamily: fontFamily,
     fontSize: 24,
     letterSpacing: 0,
     lineHeight: 32,
   },
   // title
   titleLarge: {
-    fontFamily: arial,
+    fontFamily: fontFamily,
     fontSize: 22,
     letterSpacing: 0,
     lineHeight: 28,
   },
   titleMedium: {
-    fontFamily: arial,
+    fontFamily: fontFamily,
     fontSize: 16,
     letterSpacing: 0.15,
     lineHeight: 24,
   },
   titleSmall: {
-    fontFamily: arial,
+    fontFamily: fontFamily,
     fontSize: 14,
     letterSpacing: 0.1,
     lineHeight: 20,
   },
   // label
   labelLarge: {
-    fontFamily: arial,
+    fontFamily: fontFamily,
     fontSize: 14,
     letterSpacing: 0.1,
     lineHeight: 20,
   },
   labelMedium: {
-    fontFamily: arial,
+    fontFamily: fontFamily,
     fontSize: 15,
     letterSpacing: 0.5,
     lineHeight: 18,
   },
   labelSmall: {
-    fontFamily: arial,
+    fontFamily: fontFamily,
     fontSize: 10,
     letterSpacing: 0.5,
     lineHeight: 16,
   },
   // body
   bodyLarge: {
-    fontFamily: arial,
+    fontFamily: fontFamily,
     fontSize: 16,
     letterSpacing: 0.5,
     lineHeight: 24,
   },
   bodyMedium: {
-    fontFamily: arial,
+    fontFamily: fontFamily,
     fontSize: 14,
     letterSpacing: 0.25,
     lineHeight: 20,
   },
   bodySmall: {
-    fontFamily: arial,
+    fontFamily: fontFamily,
     fontSize: 12,
     letterSpacing: 0,
     lineHeight: 16,
